9bf4388251a6ab7e3c194c68d8944c8b0ca37d7c
illia1304 <1304illia@gmail.com>
2023-09-06 11:48:17 +0300

    Add project files.

---
+++ Lesson3/Program.cs
@@ -0,0 +1,36 @@
+﻿//FIRST TASK
+Console.WriteLine("Enter number x:");
+int x;
+bool NumberX = int.TryParse(Console.ReadLine(), out x);
+Console.WriteLine("Enter number y:");
+int y;
+bool NumberY = int.TryParse(Console.ReadLine(), out y);
+
+if(!NumberX || !NumberY)
+{
+    Console.WriteLine("You didn't enter a number");
+    Console.WriteLine("I refuse to work!!!");
+    return;
+}
+
+int sum = 0;
+
+if (x == y)
+    sum = x;
+else if(x > y)
+{
+    for(int i = y; i <= x; i++)
+    {
+        sum += i;
+    }
+}
+else
+{
+    for(int i = x; i <= y; i++)
+    {
+        sum += i;
+    }
+}
+
+Console.WriteLine(sum);
+
