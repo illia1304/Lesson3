61aad137859f56a2fdf20e953f8595b58009f9f0
illia1304 <1304illia@gmail.com>
2023-09-06 11:54:32 +0300

    Second task

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -34,3 +34,40 @@ else
 
 Console.WriteLine(sum);
 
+//second task
+
+int sumO = 0;
+int sumE = 0;
+if (x == y)
+{
+    if (x % 2 == 0)
+        Console.WriteLine($"sum of even numbers = {x}");
+    else
+        Console.WriteLine($"sum of odd numbers = {x}");
+}
+    
+else if (x > y)
+{
+    for (int i = y; i <= x; i++)
+    {
+        if (i % 2 == 0)
+            sumE += i;
+        else
+            sumO += i;
+    }
+}
+else
+{
+    for (int i = x; i <= y; i++)
+    {
+        if (i % 2 == 0)
+            sumE += i;
+        else
+            sumO += i;
+    }
+}
+
+Console.WriteLine($"sum of even numbers = {sumE}");
+Console.WriteLine($"sum of odd numbers = {sumO}");
+
+
